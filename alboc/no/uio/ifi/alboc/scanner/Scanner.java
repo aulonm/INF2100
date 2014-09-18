e6f25d82d09ba61e70501c6ff1c23df975d98c91
halvorhm <h.mangseth@gmail.com@>
2014-09-18 14:32:26 +0200

    scanner read next was running inside the wrong loop.

--- alboc/no/uio/ifi/alboc/scanner/Scanner.java
+++ alboc/no/uio/ifi/alboc/scanner/Scanner.java
@@ -35,114 +35,108 @@ public class Scanner {
 		nextToken = null;
 		while (nextToken == null) {
 			nextLine = CharGenerator.curLineNum();
-
+		}
 			
-			// check if space
-			while(CharGenerator.curC == ' ') {
-				CharGenerator.readNext();
-			}
-
-			// check for comment blocks
-			if(CharGenerator.curC == '/' && CharGenerator.nextC == '*') {
-				CharGenerator.readNext();
-				CharGenerator.readNext(); // read past these two
-
-				while(! (CharGenerator.curC == '*' && CharGenerator.nextC == '/')) { // while NOT we hit */ keep skipping
-					CharGenerator.readNext();
-				}
-			}
-			
-			if (!CharGenerator.isMoreToRead()) {
-				nextToken = eofToken;
-			}
-			//first we pick up the simple ones:
-			else if(CharGenerator.curC == '+') {
-				nextToken = addToken;
-			} else if(CharGenerator.curC == '&') {
-				nextToken = ampToken;
-			} else if(CharGenerator.curC == ',') {
-				nextToken = commaToken;
-			} else if(CharGenerator.curC == '[') {
-				nextToken = leftBracketToken;
-			} else if(CharGenerator.curC == '(') {
-				nextToken = leftParToken;
-			} else if(CharGenerator.curC == '{') {
-				nextToken = leftCurlToken;
-			} else if(CharGenerator.curC == ']') {
-				nextToken = rightBracketToken;
-			} else if(CharGenerator.curC == ')') {
-				nextToken = rightParToken;
-			} else if(CharGenerator.curC == '}') {
-				nextToken = rightCurlToken;
-			} else if(CharGenerator.curC == ';') {
-				nextToken = semicolonToken;
-			} else if(CharGenerator.curC == '*') {
-				nextToken = starToken;
-			// we get a bit more advanced
-			} else if((isNumber(CharGenerator.curC)) == true) {
-				String num = ""; // initial empty string
-				while((isNumber(CharGenerator.curC)) == true) {
-					num += CharGenerator.curC;
-					CharGenerator.readNext(); // increment by one
-				}
-				nextToken = numberToken;
-				nextNum = Integer.parseInt(num); // convert string representation of the int to an integer
-			} else if(CharGenerator.curC == '=') {
-				// check if next one is equals as well
-				if(CharGenerator.nextC == '=') {
-					nextToken = equalToken; // boolean equal
-					CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
-				} else {
-					nextToken = assignToken;
-				}
-				
-			} else if(CharGenerator.curC == '>') {
-				// check if next one is equals
-				if(CharGenerator.nextC == '=') {
-					nextToken = greaterEqualToken; // boolean equal
-					CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
-				} else {
-					nextToken = greaterToken;
-				}
-			} else if(CharGenerator.curC == '<') {
-				// check if next one is equals
-				if(CharGenerator.nextC == '=') {
-					nextToken = lessEqualToken; // boolean equal
-					CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
-				} else {
-					nextToken = lessToken;
-				}
-				
-			} else if(isLetterAZ(CharGenerator.curC) == true) {
-				String name = "";
-				while(isLetterAZ(CharGenerator.curC) == true) {
-					name += CharGenerator.curC; // generate complete string
-					CharGenerator.readNext(); // increment by one
-				}
-				// now check if it's a name or something specific.
-				if(name.equals("int")){
-				}
-				else if(name.equals("for")) nextToken = forToken;
-				else if(name.equals("else")) nextToken = elseToken;
-				else if(name.equals("return")) nextToken = returnToken;
-				else if(name.equals("while")) nextToken = whileToken;
-				else {
-					nextToken = nameToken;
-					nextName = name;
-				}
-			
-
-				
-				
-			// -- Must be changed in part 0:
-
-			} else {
-				Error.error(nextLine, "Illegal symbol: '" + CharGenerator.curC
-						+ "'!");
-			}
-			Log.noteToken();
+		// check if space
+		while(CharGenerator.curC == ' ') {
 			CharGenerator.readNext();
 		}
+
+		// check for comment blocks
+		if(CharGenerator.curC == '/' && CharGenerator.nextC == '*') {
+			CharGenerator.readNext();
+			CharGenerator.readNext(); // read past these two
+
+			while(! (CharGenerator.curC == '*' && CharGenerator.nextC == '/')) { // while NOT we hit */ keep skipping
+				CharGenerator.readNext();
+			}
+		}
+			
+		if (!CharGenerator.isMoreToRead()) {
+			nextToken = eofToken;
+		}
+		//first we pick up the simple ones:
+		else if(CharGenerator.curC == '+') {
+			nextToken = addToken;
+		} else if(CharGenerator.curC == '&') {
+			nextToken = ampToken;
+		} else if(CharGenerator.curC == ',') {
+			nextToken = commaToken;
+		} else if(CharGenerator.curC == '[') {
+			nextToken = leftBracketToken;
+		} else if(CharGenerator.curC == '(') {
+			nextToken = leftParToken;
+		} else if(CharGenerator.curC == '{') {
+			nextToken = leftCurlToken;
+		} else if(CharGenerator.curC == ']') {
+			nextToken = rightBracketToken;
+		} else if(CharGenerator.curC == ')') {
+			nextToken = rightParToken;
+		} else if(CharGenerator.curC == '}') {
+			nextToken = rightCurlToken;
+		} else if(CharGenerator.curC == ';') {
+			nextToken = semicolonToken;
+		} else if(CharGenerator.curC == '*') {
+			nextToken = starToken;
+		// we get a bit more advanced
+		} else if((isNumber(CharGenerator.curC)) == true) {
+			String num = ""; // initial empty string
+			while((isNumber(CharGenerator.curC)) == true) {
+				num += CharGenerator.curC;
+				CharGenerator.readNext(); // increment by one
+			}
+			nextToken = numberToken;
+			nextNum = Integer.parseInt(num); // convert string representation of the int to an integer
+		} else if(CharGenerator.curC == '=') {
+			// check if next one is equals as well
+			if(CharGenerator.nextC == '=') {
+				nextToken = equalToken; // boolean equal
+				CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
+			} else {
+				nextToken = assignToken;
+			}
+			
+		} else if(CharGenerator.curC == '>') {
+			// check if next one is equals
+			if(CharGenerator.nextC == '=') {
+				nextToken = greaterEqualToken; // boolean equal
+				CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
+			} else {
+				nextToken = greaterToken;
+			}
+		} else if(CharGenerator.curC == '<') {
+			// check if next one is equals
+			if(CharGenerator.nextC == '=') {
+				nextToken = lessEqualToken; // boolean equal
+				CharGenerator.readNext(); // we use both chars in token, need to increment the chargenerator
+			} else {
+				nextToken = lessToken;
+			}
+			
+		} else if(isLetterAZ(CharGenerator.curC) == true) {
+			String name = "";
+			while(isLetterAZ(CharGenerator.curC) == true) {
+				name += CharGenerator.curC; // generate complete string
+				CharGenerator.readNext(); // increment by one
+			}
+			// now check if it's a name or something specific.
+			if(name.equals("int")){
+			}
+			else if(name.equals("for")) nextToken = forToken;
+			else if(name.equals("else")) nextToken = elseToken;
+			else if(name.equals("return")) nextToken = returnToken;
+			else if(name.equals("while")) nextToken = whileToken;
+			else {
+				nextToken = nameToken;
+				nextName = name;
+			}
+			// done checking the tokens
+		} else { // catch illegal tokens
+			Error.error(nextLine, "Illegal symbol: '" + CharGenerator.curC
+					+ "'!");
+		}
+		Log.noteToken();
+		CharGenerator.readNext();
 	}
 	
 	private static boolean isNumber(char c) {
